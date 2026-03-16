cc7b1020b3366fa8cf2856a6d5430164d8df94fe
MohamedBouadi <mohamed.bouadi@lexsi.ai>
2026-03-16 14:16:10 +0100

    Update checkpoint version

--- tabtune/models/orion_msp/sklearn/classifier.py
+++ tabtune/models/orion_msp/sklearn/classifier.py
@@ -166,7 +166,7 @@ class OrionMSPClassifier(ClassifierMixin, BaseEstimator):
         batch_size: Optional[int] = 8,
         model_path: Optional[str | Path] = None,
         allow_auto_download: bool = True,
-        checkpoint_version: str = "OrionMSP-classifier-v1.0-202510.ckpt",
+        checkpoint_version: str = "Orion-MSP-v1.0.ckpt",
         device: Optional[str | torch.device] = None,
         random_state: int | None = 42,
         n_jobs: Optional[int] = None,
@@ -222,7 +222,7 @@ class OrionMSPClassifier(ClassifierMixin, BaseEstimator):
 
         repo_id = "Lexsi/Orion-MSP"
         filename = self.checkpoint_version
-        ckpt = "OrionMSP-classifier-v1.0-202510.ckpt"
+        ckpt = "Orion-MSP-v1.0.ckpt"
 
         if filename == ckpt:
             info_message = (
